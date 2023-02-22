7df960fa572fd2c42a8e80b52ae60d7f37945061
Thoriso1017 <thorisoika10@gmail.com>
2023-02-22 15:25:44 -0800

    task three update

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let nam = "Thoriso";
+let surName = "Ika";
+document.getElementById("fullName").innerHTML = nam + " " + surName;
+
+let bio = "I'am passionate about implementing and launching new projects <br /> with impeccable attention to detail. My aim is to make a difference <br /> in my work place through my positive attitude.";
+document.getElementById("bioGraphy").innerHTML = bio;
+
+let addr = "65 Luthuli Park Phase 1 <br /> Seshego <br /> Polokwane, Limpopo <br /> 0700";
+document.getElementById("addr1").innerHTML = addr;
+
+document.getElementById("headr").innerHTML = "GET IN TOUCH";
+
+let phone = "079 756 8240";
+document.getElementById("phoneNum").innerHTML = phone;
+
+let email = "thorisoika10@gmail.com";
+document.getElementById("mail").innerHTML = email;
+
+document.getElementById("demo").innerHTML =
+"65 Luthuli Park Phase 1 <br /> Seshego <br /> Polokwane, Limpopo <br /> 0700";
+
+document.getElementById("adr").innerHTML = "65 Luthuli Park Phase 1 Seshego <br /> Polokwane, Limpopo <br /> 0700";
+alert('Hi. Come and see my portfolio');
+
